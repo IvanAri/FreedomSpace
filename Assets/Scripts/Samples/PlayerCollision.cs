a0dad60c5460ca9ccb8aaf82422985a95e9adb10
IvanAri <arisami.oni@gmail.com>
2018-06-28 08:40:11 +0300

    Теперь с правильными путями

---
+++ Assets/Scripts/Samples/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    // Collision event
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<ExampleGameManager>().EndGame();
+        }
+
+    }
+	
+}
