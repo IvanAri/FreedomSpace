b1d92e4e7845b1706bf60640c84040d23da070aa
IvanAri <arisami.oni@gmail.com>
2018-06-28 08:26:23 +0300

    Initial master commit

---
+++ Scripts/Controls/SimplePlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour {
+
+    public float hSpeed = 150.0f;
+    public float vSpeed = 3.0f;
+
+	// Update is called once per frame
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * hSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * vSpeed;
+
+        if (Input.GetButton("Up"))
+        {
+            print("You've inputed Up");
+        }
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+		
+	}
+}
---
+++ Scripts/Samples/EndTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour {
+
+    public ExampleGameManager gameManager;
+
+    // Way to triggering something
+	void OnTriggerEnter()
+    {
+
+        gameManager.CompleteLevel();
+
+    }
+
+}
