8ebdf23fa1b958742f6c27107284f650892bc97a
IvanAri <arisami.oni@gmail.com>
2018-06-29 22:50:40 +0300

    Простое управление осью после мучений с C# =)

---
+++ Assets/Scripts/Controls/AxisTesting/AxisController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+
+public class AxisController : MonoBehaviour {
+
+    public float verticalRotationSpeed = 10f;
+    public float horizontalRotationSpeed = 10f;
+
+    public float smooth = 5.0f;
+    public float tiltAngle = 60.0f;
+
+    public Transform mainAxis;
+
+    // Update is called once per frame
+    void Update () {
+
+        float tiltAroundZ = 0;
+        float tiltAroundY = 0;
+
+        if (Input.GetKey("s"))
+        {
+            tiltAroundZ += tiltAngle;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            tiltAroundZ -= tiltAngle;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            tiltAroundY += tiltAngle;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            tiltAroundY -= tiltAngle;
+        }
+
+        //Here we are gonna to look for an imput
+
+        //And then we would call something like RotateAxis
+        RotateAxis(tiltAroundZ, tiltAroundY);
+
+
+    }
+
+    void RotateAxis (float tiltZ, float tiltY)
+    {
+        if(tiltZ == 0f && tiltY == 0f)
+        {
+            return;
+        }
+
+        Vector3 previousAngles = transform.rotation.eulerAngles;
+
+        Quaternion target = Quaternion.Euler(0, tiltY + previousAngles.y, tiltZ + previousAngles.z);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+    }
+}
