8873563c0419572e01e8f774e4b404b182ae5686
IvanAri <arisami.oni@gmail.com>
2018-06-30 12:47:13 +0300

    Простенькое управление камерой. Позже попробую на её основе сделать что-нибудь получше.

---
+++ Assets/Scripts/Controls/AxisTesting/BetterCameraController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class BetterCameraController : MonoBehaviour {
+
+    protected Transform xFormCamera;
+    protected Transform xFormParent;
+
+    protected Vector3 localRotation;
+    protected float cameraDistance = 10f;
+
+    public float mouseSensitivity = 4f;
+    public float scrollSensitivity = 2f;
+    public float OrbitDampening = 10f;
+    public float scrollDampening = 6f;
+
+    public bool cameraDisabled = false;
+
+    // Use this for initialization
+    void Start () {
+        xFormCamera = transform;
+        xFormParent = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            cameraDisabled = !cameraDisabled;
+        }
+
+        if (!cameraDisabled)
+        {
+            // Rotation of the camera on mouse coordinates
+            if(Input.GetAxis("Mouse X") != 0f || Input.GetAxis("Mouse Y") != 0f)
+            {
+                localRotation.x += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+                localRotation.y -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+                // Clamp the rotation
+                localRotation.y = Mathf.Clamp(localRotation.y, -90f, 90f);
+            }
+        }
+
+        // Scrolling input for zoom in and out
+        if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            float scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity * Time.deltaTime;
+
+            //Makes camera zoom faster
+            scrollAmount *= (cameraDistance * 0.3f);
+
+            cameraDistance += scrollAmount * -1f;
+
+            cameraDistance = Mathf.Clamp(cameraDistance, 1.5f, 100f);
+        }
+
+        // Actuall camera rig transformation
+        Quaternion QT = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+        xFormParent.rotation = Quaternion.Lerp(xFormParent.rotation, QT, Time.deltaTime * OrbitDampening);
+
+        if(xFormCamera.localPosition.z != cameraDistance * -1f)
+        {
+            xFormCamera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(xFormCamera.localPosition.z, cameraDistance * -1f, Time.deltaTime * scrollDampening));
+        }
+
+	}
+}
