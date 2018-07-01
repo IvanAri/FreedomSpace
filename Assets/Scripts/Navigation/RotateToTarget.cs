2f78352815ee0a5ad50cf4403f892e6579d43c23
IvanAri <arisami.oni@gmail.com>
2018-07-01 15:27:01 +0300

    Добавляю ряд ассетов для сборки сцен. Без meta файлов.

---
+++ Assets/Scripts/Navigation/RotateToTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateToTarget : MonoBehaviour {
+
+    public Transform navAxis;
+    public float rotationSpeed = 100f;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.rotation = Quaternion.Slerp(transform.rotation, navAxis.rotation, Time.deltaTime * rotationSpeed);
+	}
+}
