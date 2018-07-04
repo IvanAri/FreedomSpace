2c88517c65481bf6b7d6002725e9528ff7ad2aa6
ascherbakhov <a.scherbakhov@gmail.com>
2018-07-05 01:16:32 +0300

    Added simple NetworkScene with network sync

---
+++ Assets/Scripts/Controls/PlayerController/ShipNetworkController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public static class Constants
+{
+    public const string PREFAB_AXIS = "Prefabs/NavAxis";
+    public const float MAX_SPEED = 5f;
+    public const float CHANGE_SPEED = 0.1f;
+}
+
+
+public class ShipNetworkController : NetworkBehaviour
+{
+    public GameObject axisObj;
+    public Transform navAxis;
+
+    public float rotationSpeed = 100f;
+    public float currentSpeed = 0f;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            this.transform.Find("PivotPoint").gameObject.SetActive(false);
+            return;
+        }
+        GameObject axisObj = Instantiate(Resources.Load<GameObject>(Constants.PREFAB_AXIS));
+        axisObj.GetComponent<AxisController>().mainAxis = this.GetComponent<Transform>();
+        navAxis = axisObj.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        handleInput();
+        transform.position += transform.forward *  currentSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
+        transform.rotation = Quaternion.Slerp(transform.rotation, navAxis.rotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(axisObj);
+        axisObj = null;
+        navAxis = null;
+    }
+
+    void handleInput()
+    {
+        if (Input.GetKey("r"))
+        {
+            currentSpeed = Mathf.Min(Constants.MAX_SPEED, currentSpeed + Constants.CHANGE_SPEED);
+        }
+
+        if (Input.GetKey("f"))
+        {
+            currentSpeed = Mathf.Max(0f, currentSpeed - Constants.CHANGE_SPEED);
+        }
+    }
+}
