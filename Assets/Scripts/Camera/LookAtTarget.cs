9f38b32290587214cf0cf56e8ec72dd8142f8624
IvanAri <arisami.oni@gmail.com>
2018-08-25 16:58:41 +0300

    Делаем автоматизированную камеру для демо-сцены за UI. И может пригодится для кат.сцен и всякого другого.

---
+++ Assets/Scripts/Camera/LookAtTarget.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour {
+
+
+    public Transform target = null;
+    public float rotationMultiplier = 2.0f;
+    public bool extendedLogging = false;
+    Vector3 targetPoint;
+    Quaternion targetRotation;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // Unity built-in
+        CustomLookAt();
+
+	}
+
+    void CustomLookAt()
+    {
+        if (target != null) {
+
+            targetPoint = new Vector3(target.position.x, transform.position.y, target.position.z) - transform.position;
+            targetRotation = Quaternion.LookRotation(targetPoint, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationMultiplier);
+
+        }
+        else if(extendedLogging)
+        {
+            Debug.Log("No target to look at");
+        }
+    }
+
+    // Method to provide targets
+    public void ReceiveTarget(Transform newTarget)
+    {
+        target = newTarget;
+        Debug.Log("New target assinged");
+    }
+
+    
+}
---
+++ Assets/Scripts/Camera/TestInputHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputHandler : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        // here we are going to read players input
+
+        if (Input.GetKey(KeyCode.R)) {
+
+            Transform target = GameObject.Find("RedCube").transform;
+            this.GetComponentInParent<LookAtTarget>().ReceiveTarget(target);
+
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+
+            Transform target = GameObject.Find("GreenCube").transform;
+            this.GetComponentInParent<LookAtTarget>().ReceiveTarget(target);
+
+        }
+
+        if (Input.GetKey(KeyCode.B))
+        {
+
+            Transform target = GameObject.Find("BlueCube").transform;
+            this.GetComponentInParent<LookAtTarget>().ReceiveTarget(target);
+
+        }
+
+    }
+
+
+}
