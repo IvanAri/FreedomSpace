c1b4db391eb3f0b0c513b5c49d1db375754f78ae
IvanAri <arisami.oni@gmail.com>
2018-08-26 19:48:36 +0300

    Пытаемся в кривенький UI. Совет дня - сначала гугли и учись, а потом делай

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum MainMenuButtons
+{
+    Tutorial = 0,
+    Customization = 1,
+    Multiplayer = 2,
+    Quit = 3,
+}
+
+public class MainMenuUI : MonoBehaviour {
+
+    const int MAIN_MENU_BUTTONS_COUNT = 4;
+    GameObject[] buttonsList = null;
+
+    // Use this for initialization
+    void Start () {
+		
+        buttonsList = GameObject.FindGameObjectsWithTag("button");
+
+        
+
+	}
+
+    void SortButtonsByHeight()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
